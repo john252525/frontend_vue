b619dc3dc16c9a7ae306a01e834fa60dc20aeb04
helly <maksim.birykov.2007@mail.ru>
2025-04-15 21:33:29 +0500

    test commit

--- src/composables/getDomen.js
+++ src/composables/getDomen.js
@@ -6,7 +6,7 @@ const stationDomen = reactive({
   navigate: {
     value: "",
   },
-  сosmetics: {
+  cosmetics: {
     logo: "",
     titleLogo: "",
     urlLogo: "",
@@ -19,23 +19,23 @@ export function useDomain() {
 
     if (currentDomain.value === "app4.developtech.ru") {
       stationDomen.navigate.value = "settings";
-      stationDomen.сosmetics.logo = "Crypto";
-      stationDomen.сosmetics.titleLogo = "Crypto";
-      stationDomen.сosmetics.urlLogo = "/crypto.svg";
+      stationDomen.cosmetics.logo = "Crypto";
+      stationDomen.cosmetics.titleLogo = "Crypto";
+      stationDomen.cosmetics.urlLogo = "/crypto.svg";
     }
 
     if (currentDomain.value === "app2.developtech.ru") {
       stationDomen.navigate.value = "whatsapi";
-      stationDomen.сosmetics.logo = "WhatsApi";
-      stationDomen.сosmetics.titleLogo = "WhatsApi";
-      stationDomen.сosmetics.urlLogo = "/whats_api.svg";
+      stationDomen.cosmetics.logo = "WhatsApi";
+      stationDomen.cosmetics.titleLogo = "WhatsApi";
+      stationDomen.cosmetics.urlLogo = "/whats_api.svg";
     }
-    
+
     if (currentDomain.value === "app1.developtech.ru") {
       stationDomen.navigate.value = "touchapi";
-      stationDomen.сosmetics.logo = "TouchApi";
-      stationDomen.сosmetics.titleLogo = "Touch-Api";
-      stationDomen.сosmetics.urlLogo =
+      stationDomen.cosmetics.logo = "TouchApi";
+      stationDomen.cosmetics.titleLogo = "Touch-Api";
+      stationDomen.cosmetics.urlLogo =
         "https://static.tildacdn.com/tild3630-6562-4930-b532-356635636363/favicon.ico";
     }
     console.log("Текущий домен:", stationDomen);
