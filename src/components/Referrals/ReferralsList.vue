dd461f0074938c0cbde2c195911741ab6f43f19d
helly <maksim.birykov.2007@mail.ru>
2025-06-27 12:45:01 +0500

    fix referrals page

--- src/components/Referrals/ReferralsList.vue
+++ src/components/Referrals/ReferralsList.vue
@@ -148,6 +148,7 @@ const getAccounts = async () => {
     );
 
     if (response.data.ok === true) {
+      loadDataStation.value = false;
       accounts.value = response.data.data.vendors;
       console.log(response.data);
     }
